605adee3176f11abfb2e979ab53b21b3209ed20f
songwei <476526447@qq.com>
2018-02-01 17:03:34 +0800

    day09-day10

--- awesome-python3-webapp/www/handlers.py
+++ awesome-python3-webapp/www/handlers.py
@@ -16,8 +16,29 @@ import logging
 @get('/')
 async def index(request):
 	logging.info('[handlers.py][index]')
-	users = await User.findAll()
+	summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+	#users = await User.findAll()
+	blogs = [
+		Blog(id='1',name='Test Blog',summary = summary,created_at=time.time()-120),
+		Blog(id='2',name='Something New',summary = summary,created_at=time.time()-3600),
+		Blog(id='3',name='Learn Swift',summary = summary,created_at=time.time()-7200)
+	]
+	'''
 	return {
 		'__template__': 'test.html',
 		'users': users
 	}
+	'''
+	return {
+		'__template__':'blogs.html',
+		'blogs':blogs
+	}
+
+@get('/api/users')
+async def api_get_users():
+	users = await User.findAll(orderBy='created_at desc')
+	for u in users:
+		u.passwd = '********'
+	logging.info('users= %s' % type(users))
+	return dict(users)
+	#return 'heheheh'
